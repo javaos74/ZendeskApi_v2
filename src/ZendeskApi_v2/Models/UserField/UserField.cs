e3ff3c3504a7523df4f66a9809005b791087c270
Hyungsoo Kim <javaos74@gmail.com>
2021-12-03 20:17:40 +0900

    charles integration with zedesk

---
+++ src/ZendeskApi_v2/Models/UserField/UserField.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ZendeskApi_v2.Models.UserField 
+{
+    public class UserField : IndividualSearchableResponseBase
+    {
+        
+    }
+}
